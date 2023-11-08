43c2d765fe49d4edff886672736b78a87cfbcd9b
Matias Guagni <matug2008@hotmail.com>
2023-11-08 18:13:35 -0300

    Renderizando un array, con js (mapeando un array)

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -9,20 +9,45 @@ export function App () {
 /*const formattedUserName = <span>@matiguagni11</span> */
 
 
-
+const users = [
+    {
+        userName: 'matiguagni11',
+        name: 'Matias Guagni',
+        isFollowing: true
+    },
+    {
+        userName: 'piasancheez',
+        name: 'Pia Sanchez',
+        isFollowing: false
+    },
+    {
+        userName: 'pepe123',
+        name: 'Pepe Reina',
+        isFollowing: true
+    },
+    {
+        userName: 'juanuti42',
+        name: 'Juan Torres',
+        isFollowing: false
+    },
+]
 
     return (
         <section className='App'>
-            <TwitterFollowCard userName="matiguagni11" initialIsFollowing={true}> 
-              Matias Guagni
-             </TwitterFollowCard>
-            
-            <TwitterFollowCard userName="piasancheez">
-              Pia Sanchez
-            </TwitterFollowCard>
-
-            
-
+            {
+            users.map(user => {
+                const{ userName, name, isFollowing} = user
+                return (
+                    <TwitterFollowCard
+                    key={userName}
+                    userName={userName}
+                    initialIsFollowing={isFollowing}
+                    >
+                    {name}   {/* va aca porque es children */}     
+                    </TwitterFollowCard>
+                    )
+                })
+            }
         </section>
           
     )
